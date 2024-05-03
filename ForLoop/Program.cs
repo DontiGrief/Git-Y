be6354226fcdced121b520cc737ca9abbbd89846
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:34:02 +0300

    test2

---
+++ ForLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GitHHYlesanne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta mingi number");
+            int msg = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("*******");
+
+            for (int i = 0; i < msg; i++)
+                {
+                    Console.WriteLine(i + 1);
+                if (msg == i + 1)
+                {
+                    Console.WriteLine("--");
+                    Console.WriteLine("Tehtud!");
+                }
+                else
+                {
+                    Console.WriteLine("--");
+                }
+            }
+        }
+    }
+}
---
+++ IfElse/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta mingi number");
+            int num = int.Parse(Console.ReadLine());
+            if (num >= 40)
+            {
+                Console.WriteLine("Number on rohkem kui 40");
+                Console.WriteLine("----------------------");
+                if (num >= 70)
+                {
+                    Console.WriteLine("Number on rohkem kui 70");
+
+                }
+                else
+                {
+                    Console.WriteLine("Number on vähem kui 70 ja rohkem kui 40");
+                }
+            } else
+            {
+
+                Console.WriteLine("Number on vähem kui 40");
+                Console.WriteLine("----------------------");
+                if (num >= 10)
+                {
+                    Console.WriteLine("Number on rohkem kui 10 ja vähem kui 40");
+
+                } else
+                {
+                    Console.WriteLine("Number on vähem kui 10");
+                }
+            }
+
+        }
+    }
+}
